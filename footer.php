fbd1e0f9cc3b4da78cc49ad6fa05c9e761c715e7
Martin Choutka <martafiixek@gmail.com>
2021-10-17 20:10:11 +0200

    Custom footer with 3 widgets

--- footer.php
+++ footer.php
@@ -1,8 +1,33 @@
 <footer>
-    Zápatí
-	<?php
-	dynamic_sidebar( "footer" )
-	?>
+    <footer class="text-center text-lg-start bg-light text-muted">
+        <section
+                class="p-4 border-bottom"
+        >
+			<?php
+			dynamic_sidebar( "footer-top" );
+			?>
+        </section>
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <div class="row mt-3">
+					<?php
+					dynamic_sidebar( "footer-middle" );
+					?>
+                </div>
+            </div>
+        </section>
+        <div class="p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            <div>
+				<?php
+				dynamic_sidebar( "footer-bottom" )
+				?>
+            </div>
+            <div class="d-flex justify-content-center gap-3">
+                <a class="" href="<?php echo home_url( "/" ); ?>">Modern Theme </a>
+                <span>© <?php echo date( "Y" ); ?></span>
+            </div>
+        </div>
+    </footer>
 </footer>
 <?php
 wp_footer();
