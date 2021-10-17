692728e25b39f9bef0caad59b927ff6cc34e3dc9
Martin Choutka <martafiixek@gmail.com>
2021-10-17 20:11:38 +0200

    Menu walker update

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ class Menu_Walker extends Walker_Nav_Menu {
 		if ( $depth === 0 ) {
 			$output .= '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">';
 		}
+		if ( $depth === 1 ) {
+			$output .= '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">';
+		}
 	}
 
 	function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
@@ -15,18 +18,33 @@ class Menu_Walker extends Walker_Nav_Menu {
 			$output .= "<li class='nav-item" . implode( " ", $item->classes ) . "'>";
 		}
 
-		if ( $item->url && $item->url != '#' && ( $args->walker->has_children == false ) ) {
+		if ( $item->url && $item->url != '#' && $depth !== 0 && ( $args->walker->has_children == false ) ) {
+			$output .= '<a class="dropdown-item" href="' . $item->url . '">';
+		} else if ( $args->walker->has_children == false ) {
+			$output .= '<span>';
+		}
+
+		if ( $item->url && $item->url != '#' && $depth == 0 && ( $args->walker->has_children == false ) ) {
 			$output .= '<a class="nav-link" href="' . $item->url . '">';
 		} else if ( $args->walker->has_children == false ) {
 			$output .= '<span>';
 		}
 
 		if ( $item->url == false && $args->walker->has_children == true ) {
+			$output .= '<a class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">';
+		}
+
+		if ( $item->url == true && $args->walker->has_children == true ) {
 			$output .= '<a class="nav-link dropdown-toggle" href="' . $item->url . '" id="navbarDropdown" role="button"
                            data-bs-toggle="dropdown" aria-expanded="false">';
 		}
 
 		$output .= $item->title;
+
+		if ( $item->url == true && $args->walker->has_children == true ) {
+			$output .= '</a>';
+		}
 
 		if ( $item->url == false && $args->walker->has_children == true ) {
 			$output .= '</a>';
@@ -61,7 +79,7 @@ add_action( "init", "modern_menus" );
 
 function modern_register_styles() {
 	$version = wp_get_theme()->get( "Version" );
-	wp_enqueue_style( "modern", get_template_directory_uri() . "/style.css", array( "bootstrap" ), $version, "all" );
+	wp_enqueue_style( "modern", get_template_directory_uri() . "/style.css", array( "bootstrap" ), rand(111,9999), "all" );
 	wp_enqueue_style( "bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css", array(), "5.1.3", "all" );
 }
 
@@ -89,6 +107,36 @@ function modern_widget_areas() {
 		"name"          => "Zápatí",
 		"id"            => "footer",
 		"description"   => "Oblast zápatí",
+		"before-title"  => "",
+		"after-title"   => "",
+		"before-widget" => "",
+		"after-widget"  => "",
+	) );
+
+	register_sidebar( array(
+		"name"          => "Horní zápatí",
+		"id"            => "footer-top",
+		"description"   => "Oblast horního zápatí",
+		"before-title"  => "",
+		"after-title"   => "",
+		"before-widget" => "",
+		"after-widget"  => "",
+	) );
+
+	register_sidebar( array(
+		"name"          => "Střední zápatí",
+		"id"            => "footer-middle",
+		"description"   => "Oblast středního zápatí",
+		"before-title"  => "",
+		"after-title"   => "",
+		"before-widget" => "",
+		"after-widget"  => "",
+	) );
+
+	register_sidebar( array(
+		"name"          => "Spodní zápatí",
+		"id"            => "footer-bottom",
+		"description"   => "Oblast spodního zápatí",
 		"before-title"  => "",
 		"after-title"   => "",
 		"before-widget" => "",
